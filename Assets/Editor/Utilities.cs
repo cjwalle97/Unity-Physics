e11920709995227e186237dde7e633f267b36154
Christopher Walle <cjwalle97@gmail.com>
2017-11-01 17:51:28 -0500

    Update
    Update

--- Assets/Editor/Utilities.cs
+++ Assets/Editor/Utilities.cs
@@ -19,11 +19,16 @@ class Utilities
     public bool TestOverlap(AABB a, AABB b)
     {
         bool xCollide = false;
+        bool yCollide = false;
         if(a.m_min.x <= b.m_max.x && a.m_min.x >= b.m_min.x)
         {
             xCollide = true;
         }
-        if (xCollide == true)
+        if(a.m_min.y <= b.m_max.y && a.m_min.y >= b.m_min.y)
+        {
+            yCollide = true;
+        }
+        if (xCollide == true && yCollide == true)
         {
             return true;
         }
