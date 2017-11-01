cb41e37798a328ca67a2217e9d2e8ed0c9af6b86
Christopher Walle <cjwalle97@gmail.com>
2017-11-01 17:34:11 -0500

    Update
    Update

---
+++ Assets/Editor/Utilities.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Utilities
+{
+    public class AABB
+    {
+        public Vector2 m_min;
+        public Vector2 m_max;
+
+        public AABB(Vector2 min, Vector2 max)
+        {
+            m_min = min; m_max = max;
+        }
+
+    }
+
+    public bool TestOverlap(AABB a, AABB b)
+    {
+        bool xCollide = false;
+        if(a.m_min.x <= b.m_max.x && a.m_min.x >= b.m_min.x)
+        {
+            xCollide = true;
+        }
+        if (xCollide == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
