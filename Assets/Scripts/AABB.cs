68131f0cc46bc8145e8cbd155e0cb2b2abd2a830
Christopher Walle <cjwalle97@gmail.com>
2017-11-02 11:25:16 -0500

    Update
    Creates the AABB scriptable Object and remakes the CollsionChecker
    script.

---
+++ Assets/Scripts/AABB.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AABB")]
+
+public class AABB : ScriptableObject
+{
+    public Vector2 m_min;
+    public Vector2 m_max;
+}
---
+++ Assets/Scripts/CollisionChecker.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionChecker : MonoBehaviour
+{
+    public AABB AABBconfig;
+    public AABB test;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TestOverlap(test);
+	}
+
+    public bool TestOverlap(AABB other)
+    {
+        bool xCollide = false;
+        bool yCollide = false;
+        if (AABBconfig.m_min.x <= other.m_max.x && AABBconfig.m_min.x >= other.m_min.x)
+        {
+            xCollide = true;
+        }
+        if (AABBconfig.m_min.y <= other.m_max.y && AABBconfig.m_min.y >= other.m_min.y)
+        {
+            yCollide = true;
+        }
+        if (xCollide == true && yCollide == true)
+        {
+            Debug.Log("Collision occured");
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
