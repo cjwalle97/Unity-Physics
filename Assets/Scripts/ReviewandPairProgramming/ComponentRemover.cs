6d4d8b38d3a2fbbddfdc1aec2157382f71d85be9
Christopher Walle <cjwalle97@gmail.com>
2017-11-29 12:52:59 -0600

    Update
    Update

---
+++ Assets/Scripts/ReviewandPairProgramming/ComponentRemover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Name: ComponentRemover
+//Type: class inheriting from MonoBehaviour
+//Description: Will take in a GameObject, display all the MonoBehaviours inside it,
+//and then remove them if the user presses the Remove button
+public class ComponentRemover : MonoBehaviour
+{
+    public List<MonoBehaviour> BehavioursList;
+    public GameObject gameobject;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void GetList()
+    {
+        var ol = gameobject.GetComponents<MonoBehaviour>();
+    }
+}
