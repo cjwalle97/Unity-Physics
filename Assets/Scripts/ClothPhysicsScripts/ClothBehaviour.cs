dbd6a24950a1ba158bf7c9e6587d3ab9dd764da4
Christopher Walle <cjwalle97@gmail.com>
2018-01-11 17:55:17 -0600

    Update
    Update

--- Assets/Scripts/ClothPhysicsScripts/ClothBehaviour.cs
+++ Assets/Scripts/ClothPhysicsScripts/ClothBehaviour.cs
@@ -10,8 +10,8 @@ namespace ClothPhysics
         private List<Particle> _particles;
         private List<ParticleBehaviour> _particlebehaviours;
         private List<SpringDamperBehaviour> _springdamperbehaviours;
-        private List<AerodynamicForce> _triangles;
-        private AerodynamicForce test;
+        private List<Triangles> _triangles;
+        private Triangles test;
 
         public int _width;
         public int _height;
@@ -25,7 +25,7 @@ namespace ClothPhysics
             _particles = new List<Particle>();
             _springdamperbehaviours = new List<SpringDamperBehaviour>();
             _particlebehaviours = new List<ParticleBehaviour>();
-            _triangles = new List<AerodynamicForce>();
+            _triangles = new List<Triangles>();
             CreateParticles();
             for (int i = 0; i < _height; i++)
             {
@@ -36,21 +36,12 @@ namespace ClothPhysics
             //    CreateColumn(r);
             //}
             AlignParticles();
+            LockParticles();
             //test = new AerodynamicForce(_particles[0], _particles[1], _particles[5]);
             CreateTriangles();
-            _particles[0].locked = true;
-            _particles[4].locked = true;
-            _particles[5].locked = true;
-            _particles[9].locked = true;
-            _particles[10].locked = true;
-            _particles[14].locked = true;
-            _particles[15].locked = true;
-            _particles[19].locked = true;
-            _particles[20].locked = true;
-            _particles[24].locked = true;
             for (int i = 0; i < 32; i++)
             {
-                _triangles[i].CalculateWind();
+                _triangles[i].AeroDynamicForces();
             }
         }
 
@@ -108,6 +99,21 @@ namespace ClothPhysics
                 }
             }
         }
+
+        void LockParticles()
+        {
+            _particles[0].locked = true;
+            _particles[4].locked = true;
+            _particles[5].locked = true;
+            _particles[9].locked = true;
+            _particles[10].locked = true;
+            _particles[14].locked = true;
+            _particles[15].locked = true;
+            _particles[19].locked = true;
+            _particles[20].locked = true;
+            _particles[24].locked = true;
+        }
+
         void ObjectPositioning()
         {
             for (int i = 0; i < _height * _width; i++)
@@ -127,38 +133,38 @@ namespace ClothPhysics
             //  5 - 6 - 7 - 8 - 9
             //  | \ | \ | \ | \ |
             //  0 - 1 - 2 - 3 - 4
-            var t1 = new AerodynamicForce(_particles[0], _particles[1], _particles[5]);
-            var t2 = new AerodynamicForce(_particles[1], _particles[6], _particles[5]);
-            var t3 = new AerodynamicForce(_particles[1], _particles[2], _particles[6]);
-            var t4 = new AerodynamicForce(_particles[1], _particles[7], _particles[6]);
-            var t5 = new AerodynamicForce(_particles[2], _particles[3], _particles[7]);
-            var t6 = new AerodynamicForce(_particles[3], _particles[8], _particles[7]);
-            var t7 = new AerodynamicForce(_particles[3], _particles[4], _particles[8]);
-            var t8 = new AerodynamicForce(_particles[4], _particles[9], _particles[8]);
-            var t9 = new AerodynamicForce(_particles[5], _particles[6], _particles[10]);
-            var t10 = new AerodynamicForce(_particles[6], _particles[11], _particles[10]);
-            var t11 = new AerodynamicForce(_particles[6], _particles[7], _particles[11]);
-            var t12 = new AerodynamicForce(_particles[7], _particles[12], _particles[11]);
-            var t13 = new AerodynamicForce(_particles[7], _particles[8], _particles[12]);
-            var t14 = new AerodynamicForce(_particles[8], _particles[13], _particles[12]);
-            var t15 = new AerodynamicForce(_particles[8], _particles[9], _particles[13]);
-            var t16 = new AerodynamicForce(_particles[9], _particles[14], _particles[13]);
-            var t17 = new AerodynamicForce(_particles[10], _particles[11], _particles[15]);
-            var t18 = new AerodynamicForce(_particles[11], _particles[16], _particles[15]);
-            var t19 = new AerodynamicForce(_particles[11], _particles[12], _particles[16]);
-            var t20 = new AerodynamicForce(_particles[12], _particles[17], _particles[16]);
-            var t21 = new AerodynamicForce(_particles[12], _particles[13], _particles[17]);
-            var t22 = new AerodynamicForce(_particles[13], _particles[18], _particles[17]);
-            var t23 = new AerodynamicForce(_particles[13], _particles[14], _particles[18]);
-            var t24 = new AerodynamicForce(_particles[14], _particles[19], _particles[18]);
-            var t25 = new AerodynamicForce(_particles[15], _particles[16], _particles[20]);
-            var t26 = new AerodynamicForce(_particles[16], _particles[21], _particles[20]);
-            var t27 = new AerodynamicForce(_particles[16], _particles[17], _particles[21]);
-            var t28 = new AerodynamicForce(_particles[17], _particles[22], _particles[21]);
-            var t29 = new AerodynamicForce(_particles[17], _particles[18], _particles[22]);
-            var t30 = new AerodynamicForce(_particles[18], _particles[23], _particles[22]);
-            var t31 = new AerodynamicForce(_particles[18], _particles[19], _particles[23]);
-            var t32 = new AerodynamicForce(_particles[19], _particles[24], _particles[23]);
+            var t1 = new Triangles(_particles[0], _particles[1], _particles[5]);
+            var t2 = new Triangles(_particles[1], _particles[6], _particles[5]);
+            var t3 = new Triangles(_particles[1], _particles[2], _particles[6]);
+            var t4 = new Triangles(_particles[1], _particles[7], _particles[6]);
+            var t5 = new Triangles(_particles[2], _particles[3], _particles[7]);
+            var t6 = new Triangles(_particles[3], _particles[8], _particles[7]);
+            var t7 = new Triangles(_particles[3], _particles[4], _particles[8]);
+            var t8 = new Triangles(_particles[4], _particles[9], _particles[8]);
+            var t9 = new Triangles(_particles[5], _particles[6], _particles[10]);
+            var t10 = new Triangles(_particles[6], _particles[11], _particles[10]);
+            var t11 = new Triangles(_particles[6], _particles[7], _particles[11]);
+            var t12 = new Triangles(_particles[7], _particles[12], _particles[11]);
+            var t13 = new Triangles(_particles[7], _particles[8], _particles[12]);
+            var t14 = new Triangles(_particles[8], _particles[13], _particles[12]);
+            var t15 = new Triangles(_particles[8], _particles[9], _particles[13]);
+            var t16 = new Triangles(_particles[9], _particles[14], _particles[13]);
+            var t17 = new Triangles(_particles[10], _particles[11], _particles[15]);
+            var t18 = new Triangles(_particles[11], _particles[16], _particles[15]);
+            var t19 = new Triangles(_particles[11], _particles[12], _particles[16]);
+            var t20 = new Triangles(_particles[12], _particles[17], _particles[16]);
+            var t21 = new Triangles(_particles[12], _particles[13], _particles[17]);
+            var t22 = new Triangles(_particles[13], _particles[18], _particles[17]);
+            var t23 = new Triangles(_particles[13], _particles[14], _particles[18]);
+            var t24 = new Triangles(_particles[14], _particles[19], _particles[18]);
+            var t25 = new Triangles(_particles[15], _particles[16], _particles[20]);
+            var t26 = new Triangles(_particles[16], _particles[21], _particles[20]);
+            var t27 = new Triangles(_particles[16], _particles[17], _particles[21]);
+            var t28 = new Triangles(_particles[17], _particles[22], _particles[21]);
+            var t29 = new Triangles(_particles[17], _particles[18], _particles[22]);
+            var t30 = new Triangles(_particles[18], _particles[23], _particles[22]);
+            var t31 = new Triangles(_particles[18], _particles[19], _particles[23]);
+            var t32 = new Triangles(_particles[19], _particles[24], _particles[23]);
 
             _triangles.Add(t1);
             _triangles.Add(t2);
@@ -196,10 +202,8 @@ namespace ClothPhysics
 
     }
 
-    public class AerodynamicForce
+    public class Triangles
     {
-        private Vector3 _p = Vector3.one; //density of air
-
         private float _Cd; //drag for the object
         private float _a; //cross sectional area
         private Vector3 _n; // normal of the triangle
@@ -216,7 +220,7 @@ namespace ClothPhysics
 
         public Vector3 Force;
 
-        public AerodynamicForce(Particle p1, Particle p2, Particle p3)
+        public Triangles(Particle p1, Particle p2, Particle p3)
         {
             _r1 = p1;
             _v1 = _r1.velocity;
@@ -225,11 +229,11 @@ namespace ClothPhysics
             _r3 = p3;
             _v3 = _r3.velocity;
             _Vsurface = (_v1 + _v2 + _v3) / 3;
-            _Vair = _p;
+            _Vair = new Vector3(1.0f, 0, 1.0f);
             _v = _Vsurface - _Vair;
         }
 
-        public void CalculateWind()
+        public void AeroDynamicForces()
         {
             //n= ((r2 - r1) x (r3 - r1))/|(r2 - r1) x (r3 -r1))|
             _n = (Vector3.Cross((_r2.position - _r1.position), (_r3.position - _r1.position))) / (Vector3.Magnitude(Vector3.Cross((_r2.position - _r1.position), (_r3.position - _r1.position))));
